db590d2c720acf228a11fb4c82db9d7692ae3011
Ja1rman <49319500+Ja1rman@users.noreply.github.com>
2021-11-16 20:32:36 +0300

    Add files via upload

--- kotlin_fans/StructModel.kt
+++ kotlin_fans/StructModel.kt
@@ -1,13 +1,10 @@
-object StructModel {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val vorch = Vorchun("Ворчун", "Больной-Беглец")
-        val pulul = Pululka("Пилюлька", "Больной-Беглец")
-        val pul = Pulka("Пулька", "Больной")
-        val h = Hospital()
-        h.addPerson(vorch)
-        h.addPerson(pulul)
-        h.addPerson(pul)
-        h.start()
-    }
+fun main() {
+    val vorch = Vorchun("Ворчун", "Больной-Беглец")
+    val pulul = Pululka("Пилюлька", "Больной-Беглец")
+    val pul = Pulka("Пулька", "Больной")
+    val h = Hospital()
+    h.addPerson(vorch)
+    h.addPerson(pulul)
+    h.addPerson(pul)
+    h.start()
 }
