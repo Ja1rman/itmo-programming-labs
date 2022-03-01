6ed8b716e6d5efa0fa16bd301e3527b3a26e4b76
Roman <49319500+Ja1rman@users.noreply.github.com>
2022-03-01 19:24:09 +0300

    Add files via upload

--- lab5/entities/entities.kt
+++ lab5/entities/entities.kt
@@ -2,6 +2,16 @@ package entities
 
 import java.util.*
 
+/**
+ * data class for storing elements
+ * @param id automatic generating
+ * @param name name of element
+ * @param coordinates 2 coords of element
+ * @param minimalPoint minimal point
+ * @param difficulty one of 5 difficulty
+ * @param creationDate date then this was created. automatic generating
+ * @param discipline name and selfStudyHours
+ */
 data class LabWork(var id: Long = -1,
                    val name: String,
                    val coordinates: Coordinates,
@@ -13,6 +23,11 @@ data class LabWork(var id: Long = -1,
     override fun compareTo(other: LabWork) = ((this.minimalPoint ?: 0) - (other.minimalPoint ?: 0)).toInt()
 }
 
+/**
+ * class to hold cords
+ * @param x x cord
+ * @param y y cord
+ */
 class Coordinates(
     _x: Long,
     val y: Long) {
@@ -23,9 +38,18 @@ class Coordinates(
     }
 }
 
+/**
+ * class to hold discipline info
+ * @param name name of Discipline
+ * @param selfStudyHours quant of study hours
+ */
 data class Discipline(val name: String,
                       val selfStudyHours: Int)
 
+/**
+ * enum class to discribe Difficulty
+ * @param index equal difficulty to int
+ */
 enum class Difficulty(val index: Int) {
     VERY_EASY(0), NORMAL(1), HARD(2), VERY_HARD(3), TERRIBLE(4)
 }
