60bebbff2f23b175d560c77b50c4058c920004f8
Roman <49319500+Ja1rman@users.noreply.github.com>
2022-03-01 19:23:40 +0300

    Add files via upload

--- lab5/io/JsonConvertor.kt
+++ lab5/io/JsonConvertor.kt
@@ -4,8 +4,14 @@ import datamanager.DataManager
 import java.io.BufferedOutputStream
 import java.io.FileOutputStream;
 
+/**
+ * Object to serialize objects to json strings and save
+ */
 object JsonConvertor {
     private val mapper = jacksonObjectMapper()
+    /**
+     * Do not takes args. Info to save takes from object DataManager
+     */
     fun save() {
         val json = mapper.writeValueAsString(DataManager)
         val file = FileOutputStream(System.getenv("LABVAR"))
